030d408c3bab2e26989d97e0bf05c30704b80b64
kitu <juanmix.01@gmail.com>
2025-09-11 20:32:43 -0300

    feat: add product interfaces, reusable components, and update shop layout integration

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
-
-export default function CategoryPage() {
+import {notFound} from "next/navigation";
+interface Props {
+    params: {
+        id: string
+    }
+}
+export default function CategoryPage({params}: Props) {
+    const {id} = params;
+    if(id === 'kids'){
+        notFound();
+    }
     return (
-        <div>CategoryPage</div>
+        <div>CategoryPage {id} </div>
     )
 }
--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,10 +1,16 @@
-import {titleFont} from "@/app/config/fonts";
+import {ProductGrid, Title} from "@/components";
+import {initialData} from "@/seed/seed";
 
 export default function Home() {
+    const products = initialData.products;
   return (
-    <div className="">
-        <h1> Hola mundo </h1>
-        <h1 className={titleFont.className}> Hola mundo </h1>
-    </div>
+    <>
+        <Title
+        title="Tienda"
+        subtitle="Todos los productos"
+        className="mb-2"
+        />
+        <ProductGrid products={products} />
+    </>
   );
 }
