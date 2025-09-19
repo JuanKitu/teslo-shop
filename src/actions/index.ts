874cb3b1dacbe50e133d40afaff5646423272e52
kitu <juanmix.01@gmail.com>
2025-09-18 21:47:38 -0300

    feat: add address actions, interface, and Prisma integration for user address management

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -5,3 +5,6 @@ export * from './auth/login';
 export * from './auth/logout';
 export * from './auth/registerUser';
 export * from './country/get-countries';
+export * from './address/delete-user-address';
+export * from './address/get-user-address';
+export * from './address/set-user-address';
